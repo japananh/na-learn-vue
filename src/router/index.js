728b90baabce4a65ee8d3faa1d398b604d56e876
na-seta <anhtn6797@seta-international.vn>
2020-05-17 13:51:09 +0700

    vue-router-basics

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,34 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+// import EventCreate from "@/views/EventCreate.vue";
+import EventList from "@/views/EventList.vue";
+// import EventShow from "@/views/EventShow.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "event-list",
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/event",
+    name: "event-show",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "event-show" */ "@/views/EventShow.vue")
+  },
+  {
+    path: "/event/create",
+    name: "event-create",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "event-create" */ "@/views/EventCreate.vue")
   }
 ];
 
