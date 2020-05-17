69cf6bb11af18e00cb507beaec392330c8055dd8
japananh <japananh@gmail.com>
2020-05-17 16:18:24 +0700

    dynamic routing & history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-// import EventCreate from "@/views/EventCreate.vue";
 import EventList from "@/views/EventList.vue";
-// import EventShow from "@/views/EventShow.vue";
 
 Vue.use(VueRouter);
 
@@ -10,16 +8,17 @@ const routes = [
   {
     path: "/",
     name: "event-list",
-    component: EventList
+    component: EventList,
   },
   {
-    path: "/event",
+    path: "/event/:id",
     name: "event-show",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "event-show" */ "@/views/EventShow.vue")
+      import(/* webpackChunkName: "event-show" */ "@/views/EventShow.vue"),
+    props: true,
   },
   {
     path: "/event/create",
@@ -28,14 +27,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "event-create" */ "@/views/EventCreate.vue")
-  }
+      import(/* webpackChunkName: "event-create" */ "@/views/EventCreate.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,3 +1,9 @@
 <template>
-  <h1>Showing Event #1</h1>
+  <h1>Showing Event #{{ id }}</h1>
 </template>
+
+<script>
+export default {
+  props: ["id"],
+};
+</script>
