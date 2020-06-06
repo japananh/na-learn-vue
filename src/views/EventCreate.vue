8bbbd577fc3e907e504ac4c214da0b430e1c659f
na-seta <anhtn6797@seta-international.vn>
2020-06-06 14:33:27 +0700

    reusable BaseInput

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -7,43 +7,46 @@
       <select v-model="event.category">
         <option v-for="cat in categories" :key="cat">{{ cat }}</option>
       </select>
+
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input
-          v-model="event.title"
-          type="text"
-          placeholder="Add an event title"
-        />
-      </div>
-      <div class="field">
-        <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
-      </div>
+      <BaseInput
+        type="text"
+        placeholder="Add an event title"
+        label="Title"
+        v-model="event.title"
+        class="field"
+      />
+
+      <BaseInput
+        type="text"
+        placeholder="Add an event description"
+        label="Description"
+        v-model="event.description"
+        class="field"
+      />
+
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
-      </div>
+      <BaseInput
+        type="text"
+        placeholder="Add an event Location"
+        label="Location"
+        v-model="event.location"
+        class="field"
+      />
+
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
         <datepicker v-model="event.date" placeholder="Select a date" />
       </div>
+
       <div class="field">
         <label>Select a time</label>
         <select v-model="event.time">
           <option v-for="time in times" :key="time">{{ time }}</option>
         </select>
       </div>
+
       <input type="submit" class="button -fill-gradient" value="Submit" />
     </form>
   </div>
